e4c929defae338c0976b5b8e5ccb3f043a2aaca8
Arkadiusz Gil <arkadiusz.gil@erlang-solutions.com>
2017-04-28 11:59:48 +0200

    Integrate static video streaming with Android client

--- lib/ice_demo/stream/static.ex
+++ lib/ice_demo/stream/static.ex
@@ -14,15 +14,21 @@ defmodule ICEDemo.Stream.Static do
   * `:file` - path to a h264 encoded file to stream
   * `:ip` - target IP address
   * `:port` - target port number
+  * `:control_port` - target RTCP port
   """
   def start(opts) do
     Supervisor.start_child(ICEDemo.Stream.Supervisor, [__MODULE__, opts])
+  end
+
+  def stop(pid) do
+    Supervisor.terminate_child(ICEDemo.Stream.Supervisor, pid)
   end
 
   ## GenServer callbacks
 
   def init(opts) do
     Logger.metadata tag: "Stream.Static"
+    Process.flag :trap_exit, true
     proc = start_ffmpeg(opts)
     Logger.debug "Starting with opts: #{inspect opts}"
     {:ok, proc}
@@ -47,7 +53,9 @@ defmodule ICEDemo.Stream.Static do
     {:stop, :normal, state}
   end
 
-  def terminate(_, _), do: :ok
+  def terminate(_, proc) do
+    Porcelain.Process.stop proc
+  end
 
   ## Internals
 
@@ -55,7 +63,8 @@ defmodule ICEDemo.Stream.Static do
     Porcelain.spawn(@wrapper,
       ["ffmpeg", "-re",  "-i", opts[:file],
        "-map", "0:0", "-vcodec", "h264",
-       "-f",  "rtp", "rtp://#{opts[:ip]}:#{opts[:port]}?pkt_size=1300"],
+       "-f",  "rtp",
+       "rtp://#{opts[:ip]}:#{opts[:port]}?pkt_size=1300&rtcpport=#{opts[:control_port]}"],
       out: {:send, self()}, err: {:send, self()})
   end
 end
--- lib/ice_demo/xmpp/client.ex
+++ lib/ice_demo/xmpp/client.ex
@@ -21,15 +21,17 @@ defmodule ICEDemo.XMPP.Client do
                    turn_addr: Jerboa.Client.address,
                    turn_username: String.t,
                    turn_secret: String.t,
-                   video_file: String.t}
+                   video_file: String.t,
+                   streamer_pid: nil | pid,
+                   streamer_ref: nil | reference}
 
   def start_test do
     start jid: "streamer@erlang-solutions.com",
       password: "1234",
       host: "xmpp.erlang-solutions.com",
-      turn_addr: "127.0.0.1:12100",
+      turn_addr: "217.182.204.9:12100",
       turn_username: "streamer",
-      turn_secret: "abc",
+      turn_secret: "Zd5Pb2O2",
       video_file: "sintel.h264"
   end
 
@@ -71,19 +73,19 @@ defmodule ICEDemo.XMPP.Client do
               turn_addr: turn_addr,
               turn_username: opts[:turn_username],
               turn_secret: opts[:turn_secret],
-              video_file: opts[:video_file]}
+              video_file: opts[:video_file],
+              streamer_pid: nil,
+              streamer_ref: nil}
     {:ok, state}
   end
 
   def handle_info({:stanza, stanza}, state) do
-    case find_stream_target(state, stanza) do
-      {:ok, ip, port} ->
-        send_public_ip(state, stanza.from.full)
-        start_video_stream(state, ip, port)
-      :error ->
-        :ok
-    end
-    {:noreply, state}
+    new_state = handle_stanza(state, stanza)
+    {:noreply, new_state}
+  end
+  def handle_info({:DOWN, ref, _, _, _}, %{streamer_ref: ref} = state) do
+    new_state = %{state | streamer_pid: nil, streamer_ref: nil}
+    {:noreply, new_state}
   end
   def handle_info(_, state) do
     {:noreply, state}
@@ -91,20 +93,61 @@ defmodule ICEDemo.XMPP.Client do
 
   ## Internals
 
-  defp find_stream_target(state, stanza) do
-    ## TODO: get IP of stream target from stanza
-    bare_jid_from = "#{stanza.from.user}@#{stanza.from.server}"
-    if bare_jid_from != state.jid do
-      {:ok, "127.0.0.1", "4321"}
+  defp handle_stanza(state, stanza) do
+    with {:ok, ip, port, control_port} <- find_stream_target(state, stanza) do
+      send_public_ip(state, stanza.from.full)
+      start_video_stream(state, ip, port, control_port)
     else
-      :error
+      _ -> maybe_stop_stream(state, stanza)
     end
   end
 
+  defp find_stream_target(state, stanza) do
+    with {:ok, body} <- get_body(stanza.xml),
+         [addr, control_addr]   <- String.split(body, ";"),
+         [ip, port]  <- String.split(addr, ":"),
+         [_, control_port]  <- String.split(control_addr, ":") do
+      {:ok, ip, port, control_port}
+    end
+  end
+
+  defp maybe_stop_stream(state, stanza) do
+    with {:ok, body} <- get_body(stanza.xml),
+         true        <- body =~ ~r"stop.*" do
+      ICEDemo.Stream.Static.stop(state.streamer_pid)
+      %{state | streamer_pid: nil, streamer_ref: nil}
+    else
+      _ -> state
+    end
+  end
+
+  defp get_body(xmlel(children: children)) do
+    case find_body(children) do
+      {:ok, el} ->
+        get_data(el)
+      :error ->
+        :error
+    end
+  end
+  defp get_body(_), do: :error
+
+  defp find_body([]), do: :error
+  defp find_body([xmlel(name: "body") = el | _]), do: {:ok, el}
+  defp find_body([_ | tail]), do: find_body(tail)
+
+  defp get_data(xmlel(children: children)) do
+    data =
+      Enum.reduce(children, "",
+        fn xmlcdata(content: content), acc -> acc <> content
+           _, acc -> acc
+        end)
+    {:ok, data}
+  end
+  defp get_data(_), do: :error
+
   defp send_public_ip(state, to) do
-    ## TODO: send public IP in required format
     address = get_public_ip(state)
-    message = Stanza.message(to, "", address)
+    message = Stanza.message(to, "normal", address)
     Conn.send(state.conn, message)
   end
 
@@ -116,8 +159,14 @@ defmodule ICEDemo.XMPP.Client do
     ip |> :inet.ntoa() |> to_string()
   end
 
-  defp start_video_stream(state, ip, port) do
-    {:ok, _} = ICEDemo.Stream.Static.start file: state[:video_file], ip: ip, port: port
+  defp start_video_stream(state, ip, port, control_port) do
+    if state.streamer_pid do
+      ICEDemo.Stream.Static.stop(state.streamer_pid)
+    end
+    {:ok, pid} = ICEDemo.Stream.Static.start file: state[:video_file],
+      ip: ip, port: port, control_port: control_port
+    ref = Process.monitor(pid)
+    %{state | streamer_pid: pid, streamer_ref: ref}
   end
 
   defp parse_turn_address(addr) do
